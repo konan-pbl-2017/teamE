c603c536cd1ef9be893cb1bc9dd5395088a65521
Patton256 <s1671040@Pds.konan-u.ac.jp>
2017-12-13 14:14:16 +0900

    画面遷移の簡単なプログラムを作成・アップロード

---
+++ teamE/src/TestGame/StartPanel.java
@@ -0,0 +1,46 @@
+package TestGame;
+
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+ 
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+//　ステージセレクトの画面
+ 
+public class StartPanel extends JPanel {
+	
+    JButton btn,btn2,btn3;
+    JLabel paneltitle;
+    MainFrame mf;
+    String str;
+    
+    public StartPanel(MainFrame m,String s){
+        mf = m;
+        str = s;
+        this.setName("start");
+        this.setLayout(null);
+        this.setSize(400, 200);
+        paneltitle = new JLabel("これは"
+                +getClass().getCanonicalName()+"クラスのパネルです");
+        paneltitle.setBounds(0, 5, 400, 40);
+        this.add(paneltitle);
+        
+        btn = new JButton("ステージ選択に移動");
+        btn.setBounds(20, 50, 150, 40);
+        btn.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                pc(mf.PanelNames[1]);
+            }
+        });
+        this.add(btn);
+        
+    }
+    
+    public void pc(String str){
+        mf.PanelChange((JPanel)this, str);
+    }
+    
+}
